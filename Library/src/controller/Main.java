d0a2addcdaedc08f20c42554c25cb778684bad40
miguelalmont <malcanmo@everis.com>
2020-04-23 12:31:40 +0200

    añadido libro

--- Library/src/controller/Main.java
+++ Library/src/controller/Main.java
@@ -13,13 +13,15 @@ public class Main {
 		Book b1 = new Book("Michael Ende", "La Historia Interminable", 1979);
 		Book b2 = new Book("George Orwell", "1984", 1949);
 		Book b3 = new Book("Phillip K. Dick", "Sueñan los androides con ovejas electricas", 1968);
-
+		Book b4 = new Book("Umberto Eco", "El Nombre de la Rosa", 1980);
+		
 		Map<Integer, Book> bookMap = new HashMap<>();
 
 		bookMap.put(b1.getId(), b1);
 		bookMap.put(b2.getId(), b2);
 		bookMap.put(b3.getId(), b3);
-
+		bookMap.put(b4.getId(), b4);
+		
 		for (Book b : bookMap.values()) {
 			Helper.increaseId(b, 1000);
 			System.out.println(b);
