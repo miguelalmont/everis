2e7073d5b1a6d1cd73429a172abd475e03f2518e
miguelalmont <malcanmo@everis.com>
2020-04-23 11:29:23 +0200

    incluidos test unitarios

--- Library/src/tests/BookTest.java
+++ Library/src/tests/BookTest.java
@@ -4,10 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class LibraryTest {
+public class BookTest {
 	
 	@Test
 	void testBook() {
 		fail("Not yet implemented");
 	}
+	
 }
---
+++ Library/src/tests/HelperTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import model.Book;
+import operations.Helper;
+
+public class HelperTest {
+	
+	@Test
+	void testIncreaseId() {
+		Book b1 = new Book("Michael Ende", "La Historia Interminable", 1979);
+		Helper.increaseId(b1, 1000);
+		assertEquals(1001, b1.getId());
+	}
+}
